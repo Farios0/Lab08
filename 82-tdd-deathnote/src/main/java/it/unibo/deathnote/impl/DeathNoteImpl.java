80bc8be6140a84766e813a09b5f466ce19d6d416
Geremia Francesco Conca - geremia.conca@studio.unibo.it <geremia.conca@studio.unibo.it>
2025-11-10 12:33:12 +0100

    created a first implementation of the interface provided

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImpl.java
@@ -0,0 +1,83 @@
+package it.unibo.deathnote.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//import javafx.util.Pair;
+import java.util.Objects;
+
+import it.unibo.deathnote.api.DeathNote;
+
+public class DeathNoteImpl implements DeathNote{
+    private String currentname;
+    private Map<String, List<String>> book = new HashMap<>();
+
+    @Override
+    public String getRule(int ruleNumber) {
+        if (ruleNumber <= 0 || ruleNumber > RULES.size()) {
+            throw new IllegalArgumentException("Invalid ruleNumber");
+        }
+        return RULES.get(ruleNumber);
+    }
+
+    @Override
+    public void writeName(String name) {
+        Objects.requireNonNull(name);
+        currentname = name;
+        if (book.keySet().contains(name)){
+            throw new IllegalArgumentException("the name is already on the DeathNote");
+        }
+        book.keySet().add(name);
+    }
+
+    @Override
+    public boolean writeDeathCause(String cause) {
+        if (book.get(currentname).size() > 0){
+            throw new IllegalStateException("Put a new name before writing the death cause");
+        }
+        long time = System.currentTimeMillis();
+        book.get(currentname).add(cause);
+        long time2 = System.currentTimeMillis();
+        if(time2 - time <= 40) {
+            return true;
+        } else {
+            book.get(currentname).remove(cause);
+            book.get(currentname).add("Heart Attack");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean writeDetails(String details) {
+        if (book.get(currentname).size() > 0){
+            throw new IllegalStateException("Put a new name before writing the details");
+        }
+        long time = System.currentTimeMillis();
+        book.get(currentname).add(details);
+        long time2 = System.currentTimeMillis();
+        if(time2 - time <= 384000) {
+            return true;
+        } else {
+            book.get(currentname).remove(details);
+            book.get(currentname).add("No details provided");
+            return false;
+        }
+    }
+
+    @Override
+    public String getDeathCause(String name) {
+        return book.get(name).get(0);
+    }
+
+    @Override
+    public String getDeathDetails(String name) {
+        return book.get(name).get(1);
+    }
+
+    @Override
+    public boolean isNameWritten(String name) {
+        return book.keySet().contains(name);
+    }
+    
+}
