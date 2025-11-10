47eed50c1070dd0b769a63f63117c9d56bacab7b
Geremia Francesco Conca - geremia.conca@studio.unibo.it <geremia.conca@studio.unibo.it>
2025-11-10 13:08:06 +0100

    added a method useful to testing

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/api/DeathNote.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/api/DeathNote.java
@@ -138,4 +138,6 @@ public interface DeathNote {
      * @return true if the given name is written in this DeathNote, false otherwise
      */
     boolean isNameWritten(String name);
+
+    public boolean containsName(String name);
 }
