ab81d4b2cc09c6b93b4910b4503fa76fb6299700
Geremia Francesco Conca - geremia.conca@studio.unibo.it <geremia.conca@studio.unibo.it>
2025-11-10 13:09:21 +0100

    started to add tests, still work in progress

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,31 @@
 package it.unibo.deathnote;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.function.Executable;
+
+import org.junit.jupiter.api.Assertions.*;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
 
 class TestDeathNote {
 
+    public static void testRulesExceptions(DeathNote libro, int ruleNumber){
+        assertThrows(IllegalArgumentException.class, 
+        new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                libro.getRule(ruleNumber);
+            }
+        }
+        );
+    }
+    public static void main(){
+        DeathNote libro = new DeathNoteImpl();
+        testRulesExceptions(libro, 0);
+        testRulesExceptions(libro, -8);
+        testRulesExceptions(libro, 1880);
+        
+    }
 }
